b0c47443b57d4dc77814b25db37b0301cf0e69da
Jiří <Kingofmatrix.live@gmail.com>
2022-07-08 15:08:49 +0200

    missing flag

--- src/main/java/TileMap/Tiles/BasicTile.java
+++ src/main/java/TileMap/Tiles/BasicTile.java
@@ -3,11 +3,18 @@ package TileMap.Tiles;
 import TileMap.Player;
 import TileMap.TileMap;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class BasicTile extends Tile {
     protected boolean goal;
+
+    private Image flag;
 
     @Override
     public void draw(Graphics2D g, int y, int x) {
@@ -15,6 +22,10 @@ public class BasicTile extends Tile {
         g.fillRect(x*50,y*50,50,50);
         g.setColor(Color.GRAY);
         g.drawRect(x*50,y*50,50,50);
+
+        if(goal){
+            g.drawImage(flag,5+x*50,5+y*50,40,40,null);
+        }
     }
 
     @Override
@@ -25,6 +36,12 @@ public class BasicTile extends Tile {
     }
     public void setGoal(){
         goal = true;
+        try {
+            File f = new File("Maps\\flag.png");
+            flag = ImageIO.read(f.toURI().toURL());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public boolean isGoal() {
